56a271d0c5f4c60b9c4275f178e3d8a907c40d12
Brandon Black <brandonblack02@gmail.com>
2020-08-17 16:35:49 -0700

    Basic tic-tac-toe logic in place

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,38 +1,68 @@
 import React, { useState } from 'react';
 
 export default function GameBoard() {
-    const winningSol = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
-    const [turn, setTurn] = useState(true); // True value == player1's turn
+    const winCond = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+    const [turn, setTurn] = useState(true); // True value === player1's turn
     const [win, setWin] = useState(false);
+    const [moves, setMoves] = useState(0);
+
+    let checkSame = (arr) => {
+        let board = document.querySelector('.board').childNodes
+        if (board[arr[0]].textContent !== " " && board[arr[0]].textContent === board[arr[1]].textContent && board[arr[0]].textContent === board[arr[2]].textContent) { // 3 in a row
+            setWin(true);
+            console.log(turn ? "X is the winner" : "O is the winner")
+            board[arr[0]].classList.add("winner")
+            board[arr[1]].classList.add("winner")
+            board[arr[2]].classList.add("winner")
+        }
+    }
 
     let checkWinner = () => {
-        let board = document.querySelector('.board').childNodes
-        for (let i = 0; i < board.length; i++) {
-            console.log(board[i])
+        for (let i = 0; i < winCond.length; i++) {
+            checkSame(winCond[i])
+        }
+        if (moves >= 8) { // Tie Game
+            setWin(true)
+            console.log("Tie Game")
         }
     }
 
     let play = (e) => {
-        if (e.target.value !== "X" && e.target.value !== "O" && !win) {
+        if (e.target.textContent === " " && !win) {
             e.target.textContent = turn ? "X" : "O"
             checkWinner();
+            setMoves(moves + 1)
             setTurn(!turn)
         }
     }
 
+    let reset = () => {
+        let board = document.querySelector('.board').childNodes
+        for (let i = 0; i < board.length; i++) {
+            board[i].classList.remove("winner")
+            board[i].textContent = " "
+            setMoves(0)
+        }
+        setWin(false)
+    }
+
     return (
-        <div className="board">
-            <div className="gametile" onClick={(e) => play(e)}> </div>
-            <div className="gametile" onClick={(e) => play(e)}> </div>
-            <div className="gametile" onClick={(e) => play(e)}> </div>
+        <>
+            <div className="board">
+                <div className="gametile" onClick={(e) => play(e)}> </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
 
-            <div className="gametile" onClick={(e) => play(e)}> </div>
-            <div className="gametile" onClick={(e) => play(e)}> </div>
-            <div className="gametile" onClick={(e) => play(e)}> </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
 
-            <div className="gametile" onClick={(e) => play(e)}> </div>
-            <div className="gametile" onClick={(e) => play(e)}> </div>
-            <div className="gametile" onClick={(e) => play(e)}> </div>
-        </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
+                <div className="gametile" onClick={(e) => play(e)}> </div>
+            </div>
+
+            <button onClick={() => reset()}>Reset</button>
+        </>
     )
 }
